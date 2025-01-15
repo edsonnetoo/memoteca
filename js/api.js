013835ffc07cde6019d315dbc3d76af1fd0c730f
edsonnetoo <edson.creeper@gmail.com>
2025-01-15 17:21:45 -0300

    Aula 02 - pt1

--- js/api.js
+++ js/api.js
@@ -1,7 +1,25 @@
 const api = {
     async buscarPensamentos() {
         try {
+            //Por padrão o fetch usa o GET
             const response = await fetch('http://localhost:3000/pensamentos');
+            return await response.json();
+        }
+        catch {
+            alert("Erro ao buscar Pensamentos!");
+            throw error;
+        }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
             return await response.json();
         }
         catch {
