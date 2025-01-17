bfacbbe69fb36772c4023e8d2d24baa1cc0dbb2e
edsonnetoo <edson.creeper@gmail.com>
2025-01-17 18:45:54 -0300

    Aula 03 - Fazendo requisição PUT

--- js/api.js
+++ js/api.js
@@ -23,10 +23,38 @@ const api = {
             return await response.json();
         }
         catch {
-            alert("Erro ao buscar Pensamentos!");
+            alert("Erro ao salvar Pensamentos!");
             throw error;
         }
-    }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        }
+        catch {
+            alert("Erro ao buscar Pensamento!");
+            throw error;
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        }
+        catch {
+            alert("Erro ao editar Pensamento!");
+            throw error;
+        }
+    },
 }
 
 export default api;
