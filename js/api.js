bccce78719b2b4d17b6fc9c24d1f75a539932af4
edsonnetoo <edson.creeper@gmail.com>
2025-01-20 16:23:18 -0300

    Aula 04 - Ajustando URL

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
+const URL_BASE = "http://localhost:3000";
+
 const api = {
     async buscarPensamentos() {
         try {
             //Por padrão o fetch usa o GET
-            const response = await fetch('http://localhost:3000/pensamentos');
+            const response = await fetch(`${URL_BASE}/pensamentos`);
             return await response.json();
         }
         catch {
@@ -13,7 +15,7 @@ const api = {
 
     async salvarPensamento(pensamento) {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
                 method: "POST",
                 headers: {
                     "Content-Type" : "application/json"
@@ -30,7 +32,7 @@ const api = {
 
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
             return await response.json();
         }
         catch {
@@ -41,7 +43,7 @@ const api = {
 
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type" : "application/json"
@@ -58,7 +60,7 @@ const api = {
 
     async excluirPensamento(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
                 method: "DELETE",
             });
         }
