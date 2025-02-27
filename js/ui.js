996b3cc453c1d5add321d5b5e2b54afc06f84014
edsonnetoo <edson.creeper@gmail.com>
2025-02-27 17:59:42 -0300

    Adicionando botão favorito

--- js/ui.js
+++ js/ui.js
@@ -73,6 +73,9 @@ const ui = {
             }
         }
 
+        const botaoFavorito = document.createElement("button");
+        botaoFavorito.classList.add("botao-favorito");
+
         const iconeEditar = document.createElement("img");
         iconeEditar.src = "assets/imagens/icone-editar.png";
         iconeEditar.alt = "editar";
@@ -83,8 +86,15 @@ const ui = {
         iconeExcluir.alt = "excluir";
         botaoExcluir.appendChild(iconeExcluir);
 
+        const iconeFavorito = document.createElement("img");
+        iconeFavorito.src = "assets/imagens/icone-favoritar.svg"
+        iconeFavorito.alt = "favoritar";
+        botaoFavorito.appendChild(iconeFavorito);
+
+
         const icones = document.createElement("div");
         icones.classList.add("icones");
+        icones.appendChild(botaoFavorito);
         icones.appendChild(botaoEditar);
         icones.appendChild(botaoExcluir);
 
